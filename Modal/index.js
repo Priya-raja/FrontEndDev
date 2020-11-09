e0ebf8471ce2e08a861b77d62fa59c19180c15b6
Priya raja <priya.thevar89@gmail.com>
2020-11-09 11:54:19 +0400

    Created a interactive modal

---
+++ Modal/index.js
@@ -0,0 +1,9 @@
+document.getElementById("open-modal").addEventListener("click",function (){
+    document.getElementById("Overlay").style.display = "block";
+
+})
+
+document.getElementById("close-modal").addEventListener("click",function() {
+     document.getElementById("Overlay").style.display = "none"
+
+})
