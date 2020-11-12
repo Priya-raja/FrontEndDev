b9b4c67e324ea48c503c37ded17d74d5f51f822f
Priya raja <priya.thevar89@gmail.com>
2020-11-12 08:56:46 +0400

    Wrote javacript to grab the fields

---
+++ Dice Game/index.js
@@ -0,0 +1,17 @@
+// set the game board
+let player1Score = 0;
+let player2Scor = 0;
+let player1Turn = true;
+
+//Grab the variable and store them
+
+const player1Dice = document.getElementById("player1Dice");
+const player2Dice = document.getElementById("player2Dice");
+const player1Scoreboard = document.getElementById("player2Scoreboard");
+const player2Scoreboard = document.getElementById("player2Scoreboard");
+const message = document.getElementById("message");
+const rollBtn = document.getElementById("rollBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+let m1 = message.textContent;
+console.log(m1);
