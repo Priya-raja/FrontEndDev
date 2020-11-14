c0402e574fc0f58eccd994cdbdce6b93e9bf4264
Priya raja <priya.thevar89@gmail.com>
2020-11-14 20:40:44 +0400

    Wrote moving snake and to eat apple

--- Snake game/index.js
+++ Snake game/index.js
@@ -1,46 +1,61 @@
-const grid = document.querySelector('.grid');
-const startButton = document.getElementById('start');
+const grid = document.querySelector('.grid')
+const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
-let width = 10
+const width = 10
 
-
-function createGrid (){
-   for(let i = 0; i<100; i++) {
-    let square = document.createElement('div')
+function createGrid() {
+    //create 100 of these elements with a for loop
+    for (let i=0; i < width*width; i++) {
+     //create element
+    const square = document.createElement('div')
+    //add styling to the element
     square.classList.add('square')
+    //put the element into our grid
     grid.appendChild(square)
+    //push it into a new squares array    
     squares.push(square)
-   } 
-
+    }
 }
-
 createGrid()
+
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
-function move(){
-    //remove the last element from array
-
+function move() {
+    if (
+        (currentSnake[0] + width >= width*width && direction === width) || //if snake has hit bottom
+        (currentSnake[0] % width === width-1 && direction === 1) || //if snake has hit right wall
+        (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
+        (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+    return clearInterval(timerId)
+    
+    
+    
+    
+    
+    //remove last element from our currentSnake array
     const tail = currentSnake.pop()
-    //remove style also
+    //remove styling from last element
     squares[tail].classList.remove('snake')
     //add square in direction we are heading
     currentSnake.unshift(currentSnake[0] + direction)
-    console.log(currentSnake)
     //add styling so we can see it
     squares[currentSnake[0]].classList.add('snake')
 }
 move()
 
 let timerId = setInterval(move, 1000)
-clearInterval ()
+
 
 // 39 is right arrow
 // 38 is for the up arrow
 // 37 is for the left arrow
 // 40 is for the down arrow
+
 function control(e) {
     if (e.keyCode === 39) {
         console.log('right pressed')
