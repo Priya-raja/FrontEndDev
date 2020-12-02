3017ddef7e22b1fd44611fb6a693d9d42b91bd40
Priya raja <priya.thevar89@gmail.com>
2020-12-02 23:36:34 +0400

    Completed dev challenge to scale image

---
+++ Weekly Dev Challenge/grow image/index.js
@@ -0,0 +1,30 @@
+/* Write your code here 👇 */
+let element = document.querySelector('div.panel >img')
+element.addEventListener('click',function(){
+element.classList.toggle('grow') 
+})
+    
+
+
+    
+
+
+      
+      // Function to increase image size 
+      
+      
+
+
+/*
+
+STRETCH GOALS:
+
+1) Turn it into your own gallery, portfolio, showcase, diary, travel notes, pet photo album.
+2) Choose to make it vertical, horizontal, diagonal (why not?). Experiment! 
+3) Make it so more than one panel can be expanded at a time! The user should be able to click on all panels and they are all equally spaced. 
+
+Example gif:
+
+https://docs.google.com/presentation/d/e/2PACX-1vQabQ1b7iLGqmB_MyTaKl85CSiQVGkrYQviaef03cZZOi59IEwUahS0fDqY0JkoOjY_aaE_PgTlN6_6/pub?start=false&loop=false&delayms=3000
+
+*/
