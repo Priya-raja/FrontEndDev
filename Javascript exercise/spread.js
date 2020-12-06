4b152b189046427022bdd8deb9377a3642c4a456
Priya raja <priya.thevar89@gmail.com>
2020-12-06 10:25:53 +0400

    spread operator object

--- Javascript exercise/spread.js
+++ Javascript exercise/spread.js
@@ -1,5 +1,19 @@
  let friends = ["Arya","Priya","John"]
 
- let personalFriends = ["Diya",...friends,'Jacob']
+ let personalFriends = ["Diya",...friends,'Jacob'] //It copies from the other array.
 
  console.log(personalFriends)
+
+ let person = {
+    name: "Adam",
+    age: 25,
+    city: "Manchester"
+}
+
+let employee = {
+    ...person,
+    salary: 50000,
+    position: "Software Developer"
+}
+
+console.log(employee);
