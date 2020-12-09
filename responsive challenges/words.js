e1c812da347e4b5988a8aafdbeb9d00c165ca70c
Priya raja <priya.thevar89@gmail.com>
2020-12-09 21:35:37 +0400

    final solution of reverse

--- responsive challenges/words.js
+++ responsive challenges/words.js
@@ -2,14 +2,17 @@ function spinWords(str){
   //TODO Have fun :)
    
    const words = str.split(" ");
-   console.log(words);
    const result = [];
-  
-   if(words.length >= 5){
-     return str+= words.split("").reverse()  
-     } else {
-       return words
-     }
+   
+   for (let i= 0; i<=words.length-1; i++){
+      if(words[i].length>=5){
+        result.push(words[i].split('').reverse().join(''))     
+      } else {
+        result.push(words[i])
+      }
+    
+   
 }
-
-console.log(spinWords("Hi my name is Rihaana"))
+return result.join(' ')
+}
+console.log(spinWords("Hi my name is Rihaana"));
