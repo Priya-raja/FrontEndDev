9089861c32f958a0bd4f05085dc57da2ccb14e3a
Priya raja <priya.thevar89@gmail.com>
2020-12-09 20:29:09 +0400

    splitting into words

--- responsive challenges/words.js
+++ responsive challenges/words.js
@@ -1,8 +1,10 @@
 function spinWords(str){
   //TODO Have fun :)
-   const words = [];
-   words = str.split("")
- 
+   
+   const words = str.split(" ");
+   console.log(words);
+   const result = [];
+  
    if(words.length >= 5){
      return str+= words.split("").reverse()  
      } else {
