8b3be05dce87a3096206dc61e991f2d86ac01a54
Priya raja <priya.thevar89@gmail.com>
2020-12-11 08:21:27 +0400

    installed fetch and made it run

--- Javascript exercise/fetcheg.js
+++ Javascript exercise/fetcheg.js
@@ -1,4 +1,12 @@
 const fetch = require("node-fetch");
-fetch('https://jsonplaceholder.typicode.com/comments/1')
+fetch('https://jsonplaceholder.typicode.com/comments',{
+     method: "POST",
+     body: JSON.stringify({
+        postId: 1,
+        name: 'Dylan',
+        email: 'dylansemail310@gmail.com',
+        body: 'That was dope!'
+    })
+})
 .then(response => response.json())
 .then(data => console.log(data))
