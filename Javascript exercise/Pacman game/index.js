b9d29f796ffd61a785a92c9c495f613993170966
Priya raja <priya.thevar89@gmail.com>
2020-12-13 17:51:50 +0400

    Added the ghost to the game board

--- Javascript exercise/Pacman game/index.js
+++ Javascript exercise/Pacman game/index.js
@@ -134,13 +134,19 @@ class Ghost {
         this.className = className
         this.startIndex = startIndex
         this.speed = speed
+        this.currentIndex = startIndex
+        this.isScared = false
+        this.timerId = NaN
     }
 }
 
-ghosts = [
+
+const ghosts = [
     new Ghost('blinky', 348, 250),
     new Ghost('pinky', 376, 400),
     new Ghost('inky', 351, 300),
     new Ghost('clyde', 379, 500)
 ]
+
+ghosts.forEach(ghost => squares[ghost.startIndex].classList.add(ghost.className))
 
