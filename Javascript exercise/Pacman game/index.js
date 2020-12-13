7b7bb111a6e8b34b4f5341ea4634f70eaf78e523
Priya raja <priya.thevar89@gmail.com>
2020-12-13 17:35:50 +0400

    Added css and js for Ghost

--- Javascript exercise/Pacman game/index.js
+++ Javascript exercise/Pacman game/index.js
@@ -129,3 +129,18 @@ function pacDotEaten() {
     }
 }
 
+class Ghost {
+    constructor(className, startIndex, speed) {
+        this.className = className
+        this.startIndex = startIndex
+        this.speed = speed
+    }
+}
+
+ghosts = [
+    new Ghost('blinky', 348, 250),
+    new Ghost('pinky', 376, 400),
+    new Ghost('inky', 351, 300),
+    new Ghost('clyde', 379, 500)
+]
+
