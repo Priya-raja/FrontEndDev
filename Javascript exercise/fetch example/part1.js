bd0efc0e5f2426dc9f7c83ff0f2d1352d5999c81
Priya raja <priya.thevar89@gmail.com>
2020-12-16 10:13:49 +0400

    json example

--- Javascript exercise/fetch example/part1.js
+++ Javascript exercise/fetch example/part1.js
@@ -3,5 +3,15 @@ fetch('https://www.positivityblog.com/').then( response => {
     console.log(response.text())
 })
 .then (data => {
-    console.log(data)
+    console.log(data.json())
 })
+
+fetch('people.json')
+    .then( res => res.json())
+    .then( json => {
+        json.forEach( person => {
+            const div = document.createElement('div');
+            div.innerHTML = `${person.name} is ${person.age}`;
+            document.body.appendChild(div);
+        })
+    })
