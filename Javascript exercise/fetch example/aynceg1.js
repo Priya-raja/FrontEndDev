bcd456a082273778a7ff598e784cf91e2dfea2c5
Priya raja <priya.thevar89@gmail.com>
2020-12-17 07:58:43 +0400

    Added fetch get form data

--- Javascript exercise/fetch example/aynceg1.js
+++ Javascript exercise/fetch example/aynceg1.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 async function getPosts () {
     const postPromise = await fetch('https://jsonplaceholder.typicode.com/posts')
     const posts = await postPromise.json();
-    posts.forEach( post => {
+    posts.slice(0,5).forEach( post => {
         const title = post.title;
         const body = post.body;
         console.log(title);
