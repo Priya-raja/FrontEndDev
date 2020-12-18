75217f6d28b998912be11686b820c2762fb4336c
Priya raja <priya.thevar89@gmail.com>
2020-12-18 08:18:41 +0400

    fetch post title and body

--- Javascript exercise/fetch example/part2.js
+++ Javascript exercise/fetch example/part2.js
@@ -1,8 +1,23 @@
-const fetch = require("node-fetch");
-fetch('https://jsonplaceholder.typicode.com/photos/1') // Add the URL
-	.then(res => res.json()) // Process the data
-	.then(json => { 
-		var img = document.createElement('img');
-		img.src = json.url; // Set the image source to the image URL value
-		body.document.appendChild(img);
-	}); 
+// const fetch = require("node-fetch");
+// fetch('https://jsonplaceholder.typicode.com/photos/1') // Add the URL
+// 	.then(res => res.json()) // Process the data
+// 	.then(json => { 
+// 		var img = document.createElement('img');
+// 		img.src = json.url; // Set the image source to the image URL value
+// 		body.document.appendChild(img);
+// 	}); 
+
+	// 
+	async function getPost() {
+		const postPromise = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+		const posts = await postPromise.json();
+		const html = `
+		<h1>${posts.title}</h1>
+		<p>${posts.body}</p>
+		`
+		;
+		document.body.innerHTML = html;
+	}
+	
+	getPost();
+	
